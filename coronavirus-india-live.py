1f95dbdde0c589a1f691bab5e31885d014dd6b81
aditya dand <33375699+adityadand@users.noreply.github.com>
2020-03-23 10:13:40 +0530

    Update coronavirus-india-live.py

--- coronavirus-india-live.py
+++ coronavirus-india-live.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def start():
- page = requests.get("https://economictimes.indiatimes.com/news/politics-and-nation/coronavirus-cases-in-india-live-news-latest-updates-march21/liveblog/74740672.cms")
+ page = requests.get("https://economictimes.indiatimes.com/news/politics-and-nation/coronavirus-cases-in-india-live-news-latest-updates-march22/liveblog/74754630.cms")
  soup = BeautifulSoup(page.content, 'html.parser')
  counting = soup.find(class_="textDiv l1")
  print(counting.h1.get_text())
