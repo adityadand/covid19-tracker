345db582390a2abf4078ede0c2ae92e02eec2de1
aditya dand <33375699+adityadand@users.noreply.github.com>
2022-12-24 11:26:26 +0530

    Update coronavirus-india-live.py
    extended

--- coronavirus-india-live.py
+++ coronavirus-india-live.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 def start():
  page = requests.get("https://www.worldometers.info/coronavirus/country/india")
  soup = BeautifulSoup(page.content, 'html.parser')
- counting = soup.find(class_="maincounter-number")
- print("coronavirus total cases in india"+ counting.get_text())
- start()
+ titles = soup.find_all(class_="maincounter-number")
+ list1 = ["Total cases","Deaths","Recovered"]
+ a = 0
+ for title in titles:
+   print(list1[a]+" = "+title.text.strip())
+   a=a+1
 
 start()
